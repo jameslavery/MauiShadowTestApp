98306cb8e257b5e46284b3f6d3db08537b3b2498
James Lavery <J.Lavery@biopharmservices.com>
2023-12-22 13:27:36 +0000

    Add project files.

---
+++ ShadowTestApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShadowTestApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShadowTestApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShadowTestApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
